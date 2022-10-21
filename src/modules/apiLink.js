88318670dbf16f17272ee75f1f21cc72a438155b
anyepeter <74084998+anyepeter@users.noreply.github.com>
2022-10-21 15:13:25 +0100

    Create the api call link

---
+++ src/modules/apiLink.js
@@ -0,0 +1,4 @@
+// create the link for the api using the id generated from postman.
+const apicallLink = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/XQiTrOFWGeIirrHdS8SJ/scores/';
+
+export default apicallLink;
